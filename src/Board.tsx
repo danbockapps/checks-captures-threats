19a98979dff656d4fe3d39086c9b7c5da0313ef3
Dan Bock <danbock@gmail.com>
2022-05-18 08:21:30 -0400

    Drag to add move

--- src/Board.tsx
+++ src/Board.tsx
@@ -8,30 +8,32 @@ interface Props {
 
 const Board: FC<Props> = props => {
   const [selectedSquare, setSelectedSquare] = useState<Square>()
-  const [currentArrow, setCurrentArrow] = useState<[Square?, Square?]>()
+  const [currentArrowEnd, setCurrentArrowEnd] = useState<Square>()
 
   const position = '2r2rk1/pp2bp1p/1qb1pnp1/3nN1B1/3P4/P1NQ4/BP3PPP/2R2RK1 w Qq - 0 1'
 
-  const onSquareClick = (square: Square) => {
+  const onMoveStart = (square: Square) => setSelectedSquare(square)
+  const onMoveEnd = (square: Square) => {
     if (selectedSquare) {
+      // should always be true
+      setCurrentArrowEnd(undefined)
       const ch = new Chess(position)
       const move = ch.move({ from: selectedSquare, to: square })
       if (move) props.addMove(move.san)
       setSelectedSquare(undefined)
-    } else {
-      setSelectedSquare(square)
     }
   }
 
   return (
     <Chessboard
-      {...{ position, onSquareClick }}
-      customSquareStyles={{ [selectedSquare as string]: { backgroundColor: 'yellow' } }}
+      {...{ position }}
       boardWidth={400}
-      onTouchEnd={s => setCurrentArrow([currentArrow?.[0], s])}
-      onTouchMove={s => setCurrentArrow([currentArrow?.[0], s])}
-      onTouchStart={s => setCurrentArrow([s, undefined])}
-      customArrows={currentArrow?.[0] && currentArrow?.[1] ? [currentArrow as Square[]] : undefined}
+      onTouchEnd={onMoveEnd}
+      onTouchMove={s => s !== selectedSquare && setCurrentArrowEnd(s)}
+      onTouchStart={onMoveStart}
+      customArrows={
+        selectedSquare && currentArrowEnd ? [[selectedSquare, currentArrowEnd]] : undefined
+      }
     />
   )
 }
