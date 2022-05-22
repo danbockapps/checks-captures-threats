216514a553cbf16025aac6df91243a3416011c84
Dan Bock <danbock@gmail.com>
2022-05-22 09:42:41 -0400

    Touchscreen only

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Chip } from '@mui/material'
+import { Chip, Typography } from '@mui/material'
 import { FC, useState } from 'react'
 import './app.scss'
 import Board from './Board'
@@ -9,20 +9,25 @@ const App: FC = () => {
   const [moves, setMoves] = useState<string[]>([])
 
   return (
-    <div className='app'>
-      <div className='moves'>
-        {moves.map(m => (
-          <Chip
-            key={m}
-            label={m}
-            color='primary'
-            onDelete={() => setMoves(moves.filter(move => move !== m))}
-          />
-        ))}
+    <>
+      <div className='app'>
+        <div className='moves'>
+          {moves.map(m => (
+            <Chip
+              key={m}
+              label={m}
+              color='primary'
+              onDelete={() => setMoves(moves.filter(move => move !== m))}
+            />
+          ))}
+        </div>
+        <Board {...{ moves }} addMove={(move: string) => setMoves(dedupe([...moves, move]))} />
+        <DoneButton />
       </div>
-      <Board {...{ moves }} addMove={(move: string) => setMoves(dedupe([...moves, move]))} />
-      <DoneButton />
-    </div>
+      <div className='mouse'>
+        <Typography>This app works on touchscreen devices only. Try it on your phone!</Typography>
+      </div>
+    </>
   )
 }
 
