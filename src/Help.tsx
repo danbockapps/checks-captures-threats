9ab40db2c9d99f78b80518b4d1fa71b2ef267215
Dan Bock <danbock@gmail.com>
2022-06-08 09:51:56 -0400

    Styling for Help and make the right thing scroll

--- src/Help.tsx
+++ src/Help.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, Typography } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, Typography } from '@mui/material'
 import { FC, useContext } from 'react'
 import './help.scss'
 import HelpBoard from './HelpBoard'
@@ -14,50 +14,54 @@ const Help: FC = () => {
       onClose={() => cx.setHelpOpen(false)}
       scroll='paper'
     >
-      <Typography variant='h5'>Here's what you're looking for.</Typography>
+      <DialogContent>
+        <Typography variant='h5'>Here's what you're looking for.</Typography>
 
-      <HelpBoard
-        title='1. Checks'
-        position='6k1/6pp/5p2/2r5/6N1/5B2/5PPP/5K2 w - - 0 1'
-        customArrows={[
-          ['g4', 'h6'],
-          ['g4', 'f6'],
-          ['f3', 'd5'],
-        ]}
-      />
+        <HelpBoard
+          title='1. Checks'
+          position='6k1/6pp/5p2/2r5/6N1/5B2/5PPP/5K2 w - - 0 1'
+          customArrows={[
+            ['g4', 'h6'],
+            ['g4', 'f6'],
+            ['f3', 'd5'],
+          ]}
+        />
 
-      <HelpBoard
-        title='2. Captures'
-        position='6k1/5pp1/3q3p/p6Q/8/8/5PPP/6K1 w - - 0 1'
-        customArrows={[
-          ['h5', 'a5'],
-          ['h5', 'f7'],
-          ['h5', 'h6'],
-        ]}
-      />
+        <HelpBoard
+          title='2. Captures'
+          position='6k1/5pp1/3q3p/p6Q/8/8/5PPP/6K1 w - - 0 1'
+          customArrows={[
+            ['h5', 'a5'],
+            ['h5', 'f7'],
+            ['h5', 'h6'],
+          ]}
+        />
 
-      <HelpBoard
-        title='3. Threats with a piece worth less'
-        position='6k1/2r4p/6p1/PP6/8/2B1N3/6P1/5K2 w - - 0 1'
-        customArrows={[
-          ['b5', 'b6'],
-          ['c3', 'e5'],
-          ['e3', 'd5'],
-        ]}
-      />
+        <HelpBoard
+          title='3. Threats with a piece worth less'
+          position='2k5/2r4p/6p1/PP6/8/2B1N3/6P1/5K2 w - - 0 1'
+          customArrows={[
+            ['b5', 'b6'],
+            ['c3', 'e5'],
+            ['e3', 'd5'],
+          ]}
+        />
 
-      <HelpBoard
-        title='4. Threats of undefended pieces'
-        position='8/3k1b2/p7/8/PR4P1/8/3K1B2/7r w - - 0 1'
-        customArrows={[
-          ['b4', 'b6'],
-          ['b4', 'b1'],
-          ['b4', 'f4'],
-        ]}
-      />
+        <HelpBoard
+          title='4. Threats of undefended pieces'
+          position='8/3k1b2/p7/8/PR4P1/8/3K1B2/7r w - - 0 1'
+          customArrows={[
+            ['b4', 'b6'],
+            ['b4', 'b1'],
+            ['b4', 'f4'],
+          ]}
+        />
+      </DialogContent>
 
       <DialogActions>
-        <Button onClick={() => cx.setHelpOpen(false)}>Close</Button>
+        <Button variant='contained' color='primary' onClick={() => cx.setHelpOpen(false)}>
+          Close
+        </Button>
       </DialogActions>
     </Dialog>
   )
